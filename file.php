a9e459d8c411da6240698e29e6e2e41ae46d793d
Denys Demydenko <kawoonne@gmail.com>
2022-05-26 19:25:23 +0300

    update

--- file.php
+++ file.php
@@ -1,6 +1,29 @@
 <?php
-if($_GET['func'] == 'addWord'){
-    echo 'Word is add';
+function conn(){
+    $link = mysqli_connect("localhost", "root", "", "dictionary");
+    if ($link == false){
+        print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+    }
+    else {
+        //print("Успіх<br>");
+    }
+    return $link;
 }
 
+
+
+if($_GET['funk'] == 'addWord'){
+
+    $eng = $_GET['wordEng'];
+    $ukr = $_GET['wordUkr'];
+    // Тут буде код для добалення в базу даних
+    $sql = 'INSERT INTO test1 (engWord, ukrWord) VALUES ("'.$eng .'", "'.$ukr .'")';
+    $result = mysqli_query(conn(), $sql);
+    $text = 'Слово '.$eng.' добавлено з переводом '.$ukr;
+    echo $text;
+
+
+
+}
 ?>
+
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-import {addWordCreateText} from "./content/addWord/addWordTextContent.js";
-
 let mainDoc = document.querySelector('#main');
 let lists = document.querySelectorAll('.navigationList');
 let xhr = new XMLHttpRequest();
+mainDoc.innerHTML = 'Start text';
 
-mainDoc.innerHTML = 'Hello';
-
+// присвоюємо кожному списку меню ф-ю rout
 for(let i = 0; i < lists.length; i++){
 	lists[i].onclick = rout;
 }
@@ -20,26 +18,54 @@ function practick(){
 }
 
 function addWord(){
-	mainDoc.innerHTML = addWordCreateText()
-	console.log('func addWord');
-	// xhr.open('GET', 'http://localhost/www/Projects/Dictionary/file.php?func=addWord', true);
-	// xhr.onreadystatechange = function (){
-	// 	if(xhr.readyState === 4){
-	// 		if(xhr.status === 200 ){
-	// 			let text = xhr.responseText;
-	// 			mainDoc.innerHTML = text;
-	// 		}
-	// 	}
-	// }
-	// xhr.send();
+	mainDoc.innerHTML = addWordCreateText();
+	document.querySelector("#butInputAdd").onclick = function (){
+		senderAdd();
+	}
 }
 
 function category(){
 	console.log('func category');
 }
 
-function rout(event){
+// ф-я для кнопки додати слово( відсилає запит зі словами)
+function senderAdd(){
 
+	console.log('Function senderAdd is start');
+	let docWordEng = document.querySelector('#wordEng');
+	let docWordUkr = document.querySelector('#wordUkr');
+	let wordEng = docWordEng.value;
+	let wordUkr = docWordUkr.value;
+	let param = '&wordEng=' + wordEng + '&wordUkr=' + wordUkr;
+	let myUrl = "http://localhost/www/Projects/Dictionary/file.php?funk=addWord" + param;
+	console.log('current url: ' + myUrl);
+	xhr.open('GET', myUrl, true);
+	xhr.onreadystatechange = function (){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200 ){
+				let text = xhr.responseText + ';<br>';
+				console.log('Response text: ' + text);
+				docWordEng.value = '';
+				docWordUkr.value = '';
+				let log = document.querySelector('#logAdd');
+				log.innerHTML = '<span style="font-style: italic; font-weight: bolder">' + time() + '</span>' + text + log.innerHTML;
+				document.querySelector("#butInputAdd").onclick = function (){
+					senderAdd();
+				}
+			}
+		}
+	}
+	if(wordEng === ''|| wordUkr === ''){
+		document.querySelector('#logAdd').innerHTML = '<span style="color: darkred">' +
+			'<span style="font-style: italic; font-weight: bolder">' + time() + '</span>' + 'Не заповнені поля<br></span>' +
+			document.querySelector('#logAdd').innerHTML;
+	}else{
+		xhr.send();
+	}
+
+}
+
+function rout(event){
 	switch (event.target.innerText){
 		case 'Словник':
 			dictionary();
@@ -56,3 +82,70 @@ function rout(event){
 	}
 	
 }
+
+function time(){
+	let data = new Date();
+	let h = data.getHours();
+	let m = data.getMinutes();
+	let s = data.getSeconds();
+	if(h < 10){
+		h = '0' + h;
+	}
+	if(m < 10){
+		m = '0' + m;
+	}
+	if(s < 10){
+		s = '0' + s;
+	}
+	let text = h + ':' + m + ':' + s + ': ';
+
+
+	return text;
+}
+
+// HTML розмітка для вкладки добавлення слова
+function addWordCreateText(){
+	let text = '';
+	text += '' +
+		'<div id="containerAdd">' +
+			'<div id="nameAdd">' +
+				'Добавте слово' +
+			'</div>' +
+			'<div id="wordsAdd">' +
+				'<div id="engAdd">' +
+					'<div id="englishLabelAdd">' +
+						'<h2>English</h2>' +
+					'</div>' +
+					'<div id="englishTextAdd">' +
+						'<input type="text" id="wordEng" class="textInputAdd"/>' +
+					'</div>' +
+				'</div>' +
+				'<div id="ukrAdd">' +
+					'<div id="ukraineLabelAdd">' +
+						'<h2>Ukraine</h2>' +
+					'</div>' +
+					'<div id="englishTextAdd">' +
+						'<input type="text" id="wordUkr" class="textInputAdd"/>' +
+					'</div>' +
+				'</div>' +
+			'</div>' +
+			'<div id="butAdd">' +
+				'<input type="button" value="OK" id="butInputAdd" >' +
+			'</div>' +
+			'<div id="logAdd" style="margin-left: 20px 20px 0px 20px; max-height: 200px; overflow: auto; border: 2px orange solid">' +
+
+			'</div>' +
+		'</div>'
+	;
+	return text;
+}
+
+
+
+
+
+// для тестування
+//function testBut(dat){
+	//let wordEng = document.querySelector('#wordEng').value;
+	//console.log(dat);
+//}
